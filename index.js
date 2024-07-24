6ee4d74c88e767200901a58cdedc4698c43fe45f
raphael <rafavfx1@gmail.com>
2024-07-24 19:26:52 -0300

    passando um array

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ app.get("/:user", (req, res) => {
     var user = req.params.user
     var nacionatility = req.query['from']
 
+    var info = [
+        {age: 20, unit: 'year'},
+        {height:  169, unit: 'centimeters'},
+        {weight: 154, unit: 'pounds'}
+    ]
+
 
     res.render("home", {
         user: user,
-        from: nacionatility
+        from: nacionatility, 
+        info: info
     })
 })
 
